5f966966fe5822f45ef65d22ad67abe15040ead5
yokichan <1991616yuyi@163.com>
2017-07-04 14:59:57 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by yokichan on 04/07/2017.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
